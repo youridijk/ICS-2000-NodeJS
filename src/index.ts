10c63e8f598c2ef83c088d53dd63b6604025bd9f
youridijk <steltje.maraboe_0q@icloud.com>
2022-12-27 16:23:16 +0100

    Add types

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export {default as Hub} from "./kaku/Hub";
+
+
+export {default as Command} from "./kaku/Command";
+export {default as Cryptographer} from "./kaku/Cryptographer";
+export {default as DeviceConfigs} from "./kaku/DeviceConfigs";
+
+export {default as DeviceConfig} from './kaku/model/DeviceConfig';
+export {default as DeviceData} from './kaku/model/DeviceData';
+export {default as SmartMeterData} from './kaku/model/SmartMeterData';
+export {default as SmartMeterDataCurrent} from './kaku/model/SmartMeterDataCurrent';
+
+export {default as ColorTemperatureDevice} from './kaku/devices/ColorTemperatureDevice';
+export {default as Device} from './kaku/devices/Device';
+export {default as DimDevice} from './kaku/devices/DimDevice';
+export {default as SwitchDevice} from './kaku/devices/SwitchDevice';
