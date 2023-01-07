7ed967547a279a552b4b79436a9efee2fd417e43
Ruben Dijkhof <rubendijkhof@live.nl>
2023-01-07 10:22:45 +0100

    Created branch for debug utility development
    Added debug.js utility. At this point only the '--pull-devices' parameter (in combination with username & password) is functional

---
+++ debug.js
@@ -0,0 +1,90 @@
+const yargs = require('yargs');
+
+const argv = yargs
+    .option('username', {
+        alias: 'u',
+        description: 'Parses username',
+        type: 'string'
+    })
+    .option('password', {
+        alias: 'p',
+        description: 'Parses password',
+        type: 'string'
+    })
+    .option('pull-devices', {
+        alias: 'pd',
+        description: 'Pulls all connected devices',
+        type: 'boolean'
+    })
+    .option('get-status', {
+        alias: 'gs',
+        description: 'Gets status from a device based on given entity ID',
+        type: 'number'
+    })
+    .option('change-status', {
+        alias: 'cs',
+        description: 'Change status from a device based on given entity ID',
+        type: 'number'
+    })
+    .option('function', {
+        alias: 'f',
+        description: 'Set function [integer] for the device thats changes status',
+        type: 'number'
+    })
+    .option('value', {
+        alias: 'v',
+        description: 'Set value [integer] for the device thats changes status',
+        type: 'number'
+    })
+    .option('get-on-status', {
+        alias: 'gos',
+        description: 'Get the ON/OFF status based on given entity ID',
+        type: 'number'
+    })
+    .option('get-brightness', {
+        alias: 'gb',
+        description: 'Gets brightness based on given entity ID',
+        type: 'number'
+    })
+    .help()
+    .alias('help', 'h').argv;
+
+
+if (argv.pd) {
+    if(!(argv.u && argv.p)){
+        console.log('Please enter your credentials using the username [-u] and password [-p] parameters when using this option.')
+    }
+    else {
+        console.log(`Pulling Devices...`);
+        const hub = new Hub(argv.u, argv.p)
+        await hub.login();
+        await console.log(JSON.stringify(hub.pullDevices()));
+    }
+}
+if (argv.gs) {
+    const entity_id = argv.gs;
+    console.log(`Getting status for Entity ID ${entity_id}...`);
+}
+if (argv.cs) {
+    const entity_id = argv.cs;
+    console.log(`Changing status for Entity ID ${entity_id}...`);
+}
+if (argv.f) {
+    const changefunction = argv.f;
+    console.log(`Function ${changefunction} set!`);
+}
+if (argv.v) {
+    const value = argv.v;
+    console.log(`Set function value to ${value}.`);
+}
+if (argv.gos) {
+    const entity_id = argv.gos;
+    console.log(`Getting status of ${entity_id}...`);
+}
+if (argv.gb) {
+    const entity_id = argv.gb;
+    console.log(`Getting brightness of ${entity_id}...`);
+}
+//console.log(argv);
+
+//new Hub()
